719e1b027bb8b777f128f689a068d989ff57ce7f
WinterKwon <winterkwon7017@gmail.com>
2022-05-18 10:54:07 +0900

    이메일, 패스워드 형식 검증

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const userSchema = require('../models/newuser');
 
+const bcrypt = require('bcrypt');
+const {body, validationResult} = require('express-validator');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('blog/auth');
 });
+
+//email, password 형식 맞는지 검증하기 위해 중간인자 body()메서드 이용
+//passport.js도 같은 기능
+router.post('/signup',body('email').isEmail(), body('password').isLength({min:5}), (req,res)=>{
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    return res.status(400).json({
+      errors : errors.array()
+    })
+  }
+})
 
 module.exports = router;
