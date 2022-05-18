0540cd8fcde8a07f4a3cb7dd28feb24921a4cdb9
WinterKwon <winterkwon7017@gmail.com>
2022-05-18 11:38:30 +0900

    암호화된 비밀번호 mongoDB에 저장 확인

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -11,13 +11,25 @@ router.get('/', function(req, res, next) {
 
 //email, password 형식 맞는지 검증하기 위해 중간인자 body()메서드 이용
 //passport.js도 같은 기능
-router.post('/signup',body('email').isEmail(), body('password').isLength({min:5}), (req,res)=>{
+router.post('/signup',body('email').isEmail().withMessage('아이디는 email 형식을 따르셔야합니다'), body('password').isLength({min:5}).withMessage('비밀번호는 최소 5글자 이상입니다.'), (req,res)=>{
   const errors = validationResult(req);
   if(!errors.isEmpty()){
     return res.status(400).json({
       errors : errors.array()
     })
   }
-})
+
+  //복호화
+  const salt = bcrypt.genSaltSync(10);
+  const bcryptpw = bcrypt.hashSync(req.body.password, salt);
+
+  userSchema.create({
+    email : req.body.email,
+    password : bcryptpw
+  }).then(result => {
+    res.status(200).json(result);
+  });
+
+});
 
 module.exports = router;
